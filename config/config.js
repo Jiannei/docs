b8eae957bc2d734488c63e4761d24a5ca9a3ffd0
Jiannei <longjian.huang@foxmail.com>
2021-09-18 17:42:15 +0800

    doc: packages

--- config/config.js
+++ config/config.js
@@ -1,7 +1,33 @@
 module.exports = {
     base: "/docs/",
     lang: 'zh-CN',
-    title: '你好， VuePress ！',
-    description: '这是我的第一个 VuePress 站点',
+    title: "Jianne's docs",
+    description: "Laravel & Lumen packages",
+    theme: "@vuepress/default",
+    themeConfig: {
+        navbar: [
+            {
+                text: 'Packages',
+                children: [
+                    {
+                        text: 'laravel-response',
+                        link: '/packages/laravel-response'
+                    },
+                    {
+                        text: 'laravel-enum',
+                        link: '/packages/laravel-enum'
+                    },
+                    {
+                        text: 'laravel-logger',
+                        link: '/packages/laravel-logger'
+                    },
+                    {
+                        text: 'laravel-schedule',
+                        link: '/packages/laravel-schedule'
+                    },
+                ],
+            },
+        ],
+    },
     dest: "public",
 }
